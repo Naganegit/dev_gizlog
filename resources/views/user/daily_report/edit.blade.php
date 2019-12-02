57608eeb72b69dd9dc13227b6518f6d5e05d631b
Nagasaki <esh6gc2@gmail.com>
2019-12-02 14:59:46 +0900

    インデントの調整

--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -6,24 +6,24 @@
   <div class="container">
     {!! Form::open(['route' => ['report.update', $report->id], 'method' => 'PUT']) !!}
     <div class="form-group form-size-small{{ $errors->has('reporting_time')? ' has-error' : '' }}">
-        {!! Form::input('date', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
-        @foreach ($errors->get('reporting_time') as $message)
-        <span class="help-block">{!! $message !!}</span>
-        @endforeach
-      </div>
-      <div class="form-group{{ $errors->has('title')? ' has-error' : '' }}">
-        {!! Form::input('text', 'title', $report->title, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
-        @foreach ($errors->get('title') as $message)
-        <span class="help-block">{!! $message !!}</span>
-        @endforeach
-      </div>
-      <div class="form-group{{ $errors->has('content')? ' has-error' : '' }}">
-        {!! Form::textarea('content', $report->content, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
-        @foreach ($errors->get('content') as $message)
-        <span class="help-block">{!! $message !!}</span>
-        @endforeach
-      </div>
-      {!! Form::button('Update', ['class' => 'btn btn-success pull-right', 'type' => 'submit']) !!}
+      {!! Form::input('date', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
+      @foreach ($errors->get('reporting_time') as $message)
+      <span class="help-block">{!! $message !!}</span>
+      @endforeach
+    </div>
+    <div class="form-group{{ $errors->has('title')? ' has-error' : '' }}">
+      {!! Form::input('text', 'title', $report->title, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
+      @foreach ($errors->get('title') as $message)
+      <span class="help-block">{!! $message !!}</span>
+      @endforeach
+    </div>
+    <div class="form-group{{ $errors->has('content')? ' has-error' : '' }}">
+      {!! Form::textarea('content', $report->content, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+      @foreach ($errors->get('content') as $message)
+      <span class="help-block">{!! $message !!}</span>
+      @endforeach
+    </div>
+    {!! Form::button('Update', ['class' => 'btn btn-success pull-right', 'type' => 'submit']) !!}
     {!! Form::close() !!}
   </div>
 </div>
