e50b0c02a2f35f9a0022436e8dd1a2df3f880458
Nagasaki <esh6gc2@gmail.com>
2019-12-04 13:34:16 +0900

    バリデーションに引っ掛かった時の表示をディレクティブで書き直した

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -5,15 +5,15 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => 'report.store']) !!}
-      <div class="form-group form-size-small{{ $errors->has('reporting_time')? ' has-error' : '' }}">
+      <div class="form-group form-size-small @if($errors->has('reporting_time'))has-error @endif">
         {!! Form::input('date', 'reporting_time', today()->format('Y-m-d'), ['class' => 'form-control']) !!}
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
-      <div class="form-group{{ $errors->has('title')? ' has-error' : '' }}">
+      <div class="form-group @if($errors->has('title'))has-error @endif">
         {!! Form::text('title', null,  ['class' => 'form-control', 'placeholder' => 'Title']) !!}
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
-      <div class="form-group{{ $errors->has('content')? ' has-error' : '' }}">
+      <div class="form-group @if($errors->has('content'))has-error @endif">
         {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
         <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -5,15 +5,15 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => ['report.update', $report->id], 'method' => 'PUT']) !!}
-      <div class="form-group form-size-small{{ $errors->has('reporting_time')? ' has-error' : '' }}">
+      <div class="form-group form-size-small @if($errors->has('reporting_time'))has-error @endif">
         {!! Form::input('date', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
-      <div class="form-group{{ $errors->has('title')? ' has-error' : '' }}">
+      <div class="form-group @if($errors->has('title'))has-error @endif">
         {!! Form::text('title', $report->title, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
-      <div class="form-group{{ $errors->has('content')? ' has-error' : '' }}">
+      <div class="form-group @if($errors->has('content'))has-error @endif">
         {!! Form::textarea('content', $report->content, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
         <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
