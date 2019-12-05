f43da45d1af96a9efaf626a5ab7904e24117db95
Nagasaki <esh6gc2@gmail.com>
2019-12-05 15:38:21 +0900

    created_atとupdated_atを削除。reporting_timeはdateTimeBetweenで作成するように修正

--- database/factories/DailyReportFactory.php
+++ database/factories/DailyReportFactory.php
@@ -5,11 +5,9 @@ use App\Models\DailyReport as Report;
 
 $factory->define(Report::class, function (Faker $faker) {
     return [
-        'user_id'           => $faker->numberBetween(1, 3),
+        'user_id'           => $faker->numberBetween(1, 4),
         'title'             => $faker->word,
         'content'           => $faker->paragraph,
-        'reporting_time'    => $faker->date('2019-m-d').' '.$faker->time('H:i:s'),
-        'created_at'        => $faker->date('2019-m-d').' '.$faker->time('H:i:s'),
-        'updated_at'        => $faker->date('2019-m-d').' '.$faker->time('H:i:s'),
+        'reporting_time'    => $faker->dateTimeBetween('-2 years'),
     ];
 });
